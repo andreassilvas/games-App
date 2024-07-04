edef042831526c122cf6d4351eb1749cad4cbe49
Andrea Silva <andreassilvas@hotmail.com>
2024-07-04 00:06:15 -0400

    Search Input game

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,21 +1,42 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import React from "react";
+import { useForm } from "react-hook-form";
+import {
+  FormControl,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface SearchInputProps {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: SearchInputProps) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement color="green.800" children={<BsSearch />} />
-      <Input
-        borderRadius={20}
-        placeholder="Search games..."
-        variant={"fille"}
-        bgGradient="linear(to-r, green.200, pink.500)"
-        color="blackAlpha.800"
-        fontWeight="500"
-        _placeholder={{ color: "blackAlpha.500" }}
-      />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <FormControl>
+        <InputGroup>
+          <InputLeftElement color="green.800" children={<BsSearch />} />
+          <Input
+            ref={ref}
+            borderRadius={20}
+            placeholder="Search games..."
+            variant={"fille"}
+            bgGradient="linear(to-r, green.200, pink.500)"
+            color="blackAlpha.800"
+            fontWeight="500"
+            _placeholder={{ color: "blackAlpha.500" }}
+          />
+        </InputGroup>
+      </FormControl>
+    </form>
   );
 };
 
