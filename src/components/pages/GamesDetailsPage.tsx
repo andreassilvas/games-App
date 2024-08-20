ce0a09eabc9f1bcb8db58dcb5161f3c4d371b9bc
Andrea Silva <andreassilvas@hotmail.com>
2024-08-20 01:17:25 -0400

    Build gameAttributes

--- src/components/pages/GamesDetailsPage.tsx
+++ src/components/pages/GamesDetailsPage.tsx
@@ -1,7 +1,10 @@
-import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../../hooks/useGame";
 import ExpandableText from "../ExpandableText";
+import DefinitionItem from "../DefinitionItem";
+import CriticScore from "../CriticScore";
+import GameAttributes from "../GameAttributes";
 
 export const GamesDetailsPage = () => {
   const { slug } = useParams();
@@ -14,6 +17,31 @@ export const GamesDetailsPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game}></GameAttributes>
+
+      {/* <SimpleGrid columns={2} as="dl">
+        <DefinitionItem term="Platform">
+          {game.parent_platforms?.map(({ platform }) => (
+            <Text key={platform.id}>{platform.name}</Text>
+          ))}
+        </DefinitionItem>
+
+        <DefinitionItem term="Metascore">
+          <CriticScore score={game.metacritic}></CriticScore>
+        </DefinitionItem>
+
+        <DefinitionItem term="Genres">
+          {game.genres.map((genre) => (
+            <Text key={genre.id}>{genre.name}</Text>
+          ))}
+        </DefinitionItem>
+
+        <DefinitionItem term="Publishers">
+          {game.publishers?.map((publisher) => (
+            <Text key={publisher.id}>{publisher.name}</Text>
+          ))}
+        </DefinitionItem>
+      </SimpleGrid> */}
     </>
   );
 };
